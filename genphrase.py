98c1f22765ca2e26b15f997bd5775dd288d87d3b
stef <stefan.marsiske@gmail.com>
2012-10-02 02:53:37 +0200

    [mod] added licence

--- genphrase.py
+++ genphrase.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# (c) 2012 s@ctrlc.hu
+#
+#  This is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
 
 from Crypto import Random
 import struct, sys
